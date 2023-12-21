6e4fd5315747cbc3f6f6838d74317b98ef6fd526
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-12-21 14:35:12 +0000

    add update question call

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # %%
 from cred import get_credentials
 from service_template import form_service, drive_service, sheet_service, form_handler
+
 # %%
 # Create service instances with credentials
 form_service_instance = form_service(get_credentials())
@@ -8,8 +9,10 @@ drive_service_instance = drive_service(get_credentials())
 sheet_service_instance = sheet_service(get_credentials())
 # %%
 # Create a form
-form = form_handler(form_service_instance, form_title = "Test Form")
+form = form_handler(form_service_instance, form_title="Test Form")
 
+# %%
+update_name = form.update_form_title("New Form Name")
 # %%
 # Request body for creating a form
 NEW_FORM = {
@@ -50,63 +53,39 @@ NEW_QUESTION = {
         }
     ]
 }
-
-# # Define the form structure
-# form_body = {
-#     "title": "Your Form Title",
-#     "description": "Your Form Description",
-#     "items": [
-#         {
-#             "title": "Question 1",
-#             "type": "TEXT",
-#         },
-#         {
-#             "title": "Question 2",
-#             "type": "MULTIPLE_CHOICE",
-#             "multipleChoice": {
-#                 "choices": [
-#                     {"text": "Option 1"},
-#                     {"text": "Option 2"},
-#                     {"text": "Option 3"},
-#                 ],
-#             },
-#         },
-#         # Add more questions as needed
-#     ],
-# }
+# %%
+first_question = form.add_question(NEW_QUESTION)
+print(first_question)
+# %%
+UPDATE_QUESTION = {
+    "requests": [
+        {
+            "updateItem": {
+                "item": {
+                    "itemId": "09979f83",
+                    "questionItem": {
+                        "question": {
+                            "questionId": "00d3ea29",
+                            "required": True,
+                            "choiceQuestion": {
+                                "type": "CHECKBOX",
+                                "options": [
+                                    {"value": "1965"},
+                                    {"value": "1967"},
+                                    {"value": "1969"},
+                                    {"value": "1971"},
+                                ],
+                                "shuffle": True,
+                            },
+                        }
+                    },
+                },
+                "location": {"index": 0},
+                "updateMask": "questionItem",
+            }
+        }
+    ]
+}
 
 # %%
-# Create the form
-result = service.forms().create(body=NEW_FORM).execute()
-# Adds the question to the form
-
-# %%
-
-question_setting = (
-    service.forms()
-    .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
-    .execute()
-)
-
-# %%
-get_result = service.forms().get(formId=result["formId"]).execute()
-
-print(get_result)
-
-
-# print(f"Form created: {response['form']['formId']}")
-
-# %%
-# get the responses recorded by the form
-response = (
-    service.forms()
-    .responses()
-    .list(formId=result["formId"])
-    .execute()
-)
-print(response)
-
-
-
-
-# %%
+updated_questions = form.update_question(UPDATE_QUESTION)
