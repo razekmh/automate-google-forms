8576ae4f694ea3fd1246da71d11c7ce1b88259ad
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-12-28 18:57:16 +0000

    add new grid question builder and request builder

--- utils.py
+++ utils.py
@@ -5,3 +5,35 @@ def convert_sheet_data_to_df(sheet_data: dict) -> pd.DataFrame:
     values = sheet_data.get("values", [])
     df = pd.DataFrame(values[1:], columns=values[0])
     return df
+
+
+def build_json_for_grid_question(selection_criteria, name_of_candidate, INDEX=0):
+    answer_values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    options = [{"value": value} for value in answer_values]
+    questions = [
+        {"rowQuestion": {"title": question}} for question in selection_criteria
+    ]
+    NEW_GRID_QUESTION = {
+        "createItem": {
+            "item": {
+                "title": name_of_candidate,
+                "questionGroupItem": {
+                    "grid": {
+                        "columns": {
+                            "type": "RADIO",
+                            "options": options,
+                        }
+                    },
+                    "questions": questions,
+                },
+            },
+            "location": {"index": INDEX},
+        }
+    }
+
+    return NEW_GRID_QUESTION
+
+
+def build_requests_list(list_of_requests):
+    requests_list = {"requests": list_of_requests}
+    return requests_list
