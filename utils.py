ab0d02065428c4ac4df890950ce5fda2f27e1e60
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-12-28 21:43:34 +0000

    add base make form

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from typing import DataFrame
+from service_template import Award
+from settings import SPREADSHEET_ID, RANGE, MAJOR_DIMENSION, DOCUMENT_ID
 
 
 def convert_sheet_data_to_df(sheet_data: dict) -> pd.DataFrame:
@@ -22,6 +23,43 @@ def process_df(df: pd.DataFrame) -> pd.core.groupby.DataFrameGroupBy:
         ["Individual/Project/Alumni", "For Individual Nominations only"]
     )
     return groupdf
+
+
+def make_form(groupdf: pd.core.groupby.DataFrameGroupBy, document_service_instance):
+    types_pf_award = [index[0] for index, _ in groupdf]
+    titles_of_forms = [index[1] for index, _ in groupdf]
+    document_content = document_service_instance.get(DOCUMENT_ID)
+
+    for form_title in titles_of_forms:
+        question_json_list = []
+        if form_title == "Project":
+            award_type = "Project"
+            critria = document_service_instance.get_award_info(
+                document_content, Award.COLLABORATIVE_PROJECTS
+            )
+            dataframe = groupdf.get_group(("Project", "Project"))
+            for name in dataframe["Name"]:
+                question_json = build_json_for_grid_question(
+                    list(critria.values())[0], name
+                )
+                question_json_list.append(question_json)
+            request_body = build_requests_list(question_json_list)
+            return request_body
+
+        elif form_title == "Alumni Association":
+            award_type = "Alumni Association"
+            critria = document_service_instance.get_award_info(
+                document_content, Award.ALLUMNI_ASSOCIATIONS
+            )
+            dataframe = groupdf.get_group(("Alumni Association", "Alumni Association"))
+            pass
+
+        else:
+            award_type = "Individual"
+            critria = document_service_instance.get_award_info(
+                document_content, Award.INDIVIDUAL_APPLICATIONS
+            )
+            dataframe = groupdf.get_group(("Individual", form_title))
 
 
 def build_json_for_grid_question(selection_criteria, name_of_candidate, INDEX=0):
