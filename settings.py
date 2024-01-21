175a27677f1cbada5de366d880f7ea4a565d640c
Mahmoud Abdelrazek <razekmh@gmail.com>
2024-01-21 12:24:27 +0000

    fix import settings

--- settings.py
+++ settings.py
@@ -1,9 +1,16 @@
-SPREADSHEET_ID = "example spreadsheetId"
-RANGE = "example range"
-MAJOR_DIMENSION = "ROWS"
-DOCUMENT_ID = "example documentId"
-#
-try:
-    from local_settings import *
-except ImportError:
-    pass
+from pathlib import Path
+import os
+
+# cehck if local.settings.py exists
+if Path("local_settings.py").exists():
+    # run local.settings.py
+    from local_settings import push_local_settings_to_env
+
+    push_local_settings_to_env()
+
+SPREADSHEET_ID = os.environ.get("SPREADSHEET_ID", default="example spreadsheetId")
+RANGE = os.environ.get("RANGE", default="example range")
+MAJOR_DIMENSION = os.environ.get("MAJOR_DIMENSION", default="ROWS")
+DOCUMENT_ID = os.environ.get("DOCUMENT_ID", default="example documentId")
+DATA_DIRECTORY_PATH = Path(os.environ.get("DATA_DIRECTORY_PATH", default="/data"))
+DEBUG = os.environ.get("DEBUG", default=False)
